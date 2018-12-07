2e07d319ee039dbff5f5693af1769c0c7babb0de
Scott Bergler <commandinghands@gmail.com>
2018-12-07 12:51:29 -0800

    confirm spec test seven passes.

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -64,30 +64,27 @@ namespace HairSalon.Models
 
     public static List<Client> GetAll()
     {
-      Client newClient = new Client("Scott", "Bergler", "5038905118", 1, 1);
-      List<Client> allClients = new List<Client> {newClient};
-
-      // List<Client> allClients = new List<Client> {};
-      // MySqlConnection conn = DB.Connection();
-      // conn.Open();
-      // MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-      // cmd.CommandText = @"SELECT * FROM bottles;";
-      // MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-      // while(rdr.Read())
-      // {
-      //   int clientId = rdr.GetInt32(0);
-      //   string clientFirstName = rdr.GetString(1);
-      //   string clientLastName = rdr.GetString(2);
-      //   string clientPhoneNumber = rdr.GetString(3);
-      //   int stylistId = rdr.GetInt32(4);
-      //   Client newClient = new Client(clientFirstName, clientLastName, clientPhoneNumber, stylistId, clientId);
-      //   allClients.Add(newClient);
-      // }
-      // conn.Close();
-      // if (conn != null)
-      // {
-      //   conn.Dispose();
-      // }
+      List<Client> allClients = new List<Client> {};
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"SELECT * FROM clients;";
+      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
+      while(rdr.Read())
+      {
+        int clientId = rdr.GetInt32(0);
+        string clientFirstName = rdr.GetString(1);
+        string clientLastName = rdr.GetString(2);
+        string clientPhoneNumber = rdr.GetString(3);
+        int stylistId = rdr.GetInt32(4);
+        Client newClient = new Client(clientFirstName, clientLastName, clientPhoneNumber, stylistId, clientId);
+        allClients.Add(newClient);
+      }
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
       return allClients;
     }
 

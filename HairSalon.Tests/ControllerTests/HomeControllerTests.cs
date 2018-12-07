c2518a8b174c9723fd1e6c0ee4eab2bd50f45014
Scott Bergler <commandinghands@gmail.com>
2018-12-07 11:22:46 -0800

    confirm spec test one fails.

--- HairSalon.Tests/ControllerTests/HomeControllerTests.cs
+++ HairSalon.Tests/ControllerTests/HomeControllerTests.cs
@@ -4,21 +4,21 @@ using HairSalon.Controllers;
 
 namespace HairSalon.Tests
 {
-    [TestClass]
-    public class HomeControllerTest
-    {
-      [TestMethod]
-      public void Index_ReturnsCorrectView_True()
-      {
-          //Arrange
-        HomeController controller = new HomeController();
-
-        //Act
-        ActionResult indexView = controller.Index();
-
-        //Assert
-        Assert.IsInstanceOfType(indexView, typeof(ViewResult));
-    }
+  [TestClass]
+  public class HomeControllerTest
+  {
+    // [TestMethod]
+    // public void Index_ReturnsCorrectView_True()
+    // {
+    //   //Arrange
+    //   HomeController controller = new HomeController();
+    //
+    //   //Act
+    //   ActionResult indexView = controller.Index();
+    //
+    //   //Assert
+    //   Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+    // }
 
   }
 }

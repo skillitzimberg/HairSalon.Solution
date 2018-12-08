0cdccc0dbe286a154aa1d94337e406b9fcea0e8c
Scott Bergler <commandinghands@gmail.com>
2018-12-08 12:28:38 -0800

    confirm that spec 28 passes.

--- HairSalon/Controllers/StylistsController.cs
+++ HairSalon/Controllers/StylistsController.cs
@@ -25,7 +25,7 @@ namespace HairSalon.Controllers
     {
       Stylist newStylist = new Stylist(stylistFirstName, stylistLastName);
       List<Stylist> allStylists = Stylist.GetAll();
-      return View("Index", 0);
+      return View("Index", allStylists);
     }
 
   }

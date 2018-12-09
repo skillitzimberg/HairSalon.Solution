25063d0df594c4875fc37c64e0b0bfaa9c86549a
Scott Bergler <example@example.com>
2018-12-09 12:35:26 -0800

    uncomment tests.

--- HairSalon.Tests/ModelTests/ClientTests.cs
+++ HairSalon.Tests/ModelTests/ClientTests.cs
@@ -69,91 +69,91 @@ namespace HairSalon.Tests
 
       Assert.AreEqual(expectedClientName, actualClientName);
     }
-    //
-    // [TestMethod]
-    // public void GetPhoneNumber_ReturnsPhoneNUmber_String()
-    // {
-    //   string expectedPhoneNumber = "5038905118";
-    //   Client newClient = new Client("Scott", "Bergler", expectedPhoneNumber, 1, 1);
-    //
-    //   string actualPhoneNumber = newClient.GetPhoneNumber();
-    //
-    //   Assert.AreEqual(expectedPhoneNumber, actualPhoneNumber);
-    // }
-    //
-    // [TestMethod]
-    // public void GetStylistId_ReturnsStylistId_Int()
-    // {
-    //   int expectedStylistId = 1;
-    //   Client newClient = new Client("Scott", "Bergler", "5038905118", expectedStylistId, 1);
-    //
-    //   int actualStylistId = newClient.GetStylistId();
-    //
-    //   Assert.AreEqual(expectedStylistId, actualStylistId);
-    // }
-    //
-    // [TestMethod]
-    // public void GetAll_ReturnsEmptyList_List()
-    // {
-    //   List<Client> expectedClientList = new List<Client> { };
-    //
-    //   List<Client> actualClientList = Client.GetAll();
-    //
-    //   CollectionAssert.AreEqual(expectedClientList, actualClientList);
-    // }
-    //
-    // [TestMethod]
-    // public void Save_SavesToDatabase_ClientList()
-    // {
-    //   Client newClient = new Client("Scott", "Bergler", "5038905118", 1);
-    //   List<Client> expectedClientList = new List<Client>{newClient};
-    //
-    //   newClient.Save();
-    //   List<Client> actualClientList = Client.GetAll();
-    //
-    //   CollectionAssert.AreEqual(expectedClientList, actualClientList);
-    // }
-    //
-    // [TestMethod]
-    // public void GetAll_ReturnsAllClients_ClientList()
-    // {
-    //   Client clientOne = new Client("Scott", "Bergler", "5038905118", 1);
-    //   Client clientTwo = new Client("Millicent", "Zimdars", "5034217832", 2);
-    //   List<Client> expectedClientList = new List<Client>{ clientOne, clientTwo };
-    //
-    //   clientOne.Save();
-    //   clientTwo.Save();
-    //   List<Client> actualClientList = Client.GetAll();
-    //
-    //   CollectionAssert.AreEqual(expectedClientList, actualClientList);
-    // }
-    //
-    // [TestMethod]
-    // public void Save_AssignsId_Int()
-    // {
-    //   Client clientOne = new Client("Scott", "Bergler", "5038905118", 1);
-    //   Client clientTwo = new Client("Millicent", "Zimdars", "5034217832", 2);
-    //   List<Client> expectedClientList = new List<Client>{ clientOne, clientTwo };
-    //
-    //   clientOne.Save();
-    //   clientTwo.Save();
-    //   List<Client> actualClientList = Client.GetAll();
-    //
-    //   Assert.AreEqual(expectedClientList[1].GetId(), actualClientList[1].GetId());
-    // }
-    //
-    // [TestMethod]
-    // public void Find_ReturnsCorrectClientFromDatabase_Client()
-    // {
-    //   Client clientOne = new Client("Scott", "Bergler", "5038905118", 1);
-    //   Client clientTwo = new Client("Millicent", "Zimdars", "5034217832", 2);
-    //
-    //   clientOne.Save();
-    //   clientTwo.Save();
-    //   Client foundClient = Client.Find(clientTwo.GetId());
-    //
-    //   Assert.AreEqual(clientTwo, foundClient);
-    // }
+
+    [TestMethod]
+    public void GetPhoneNumber_ReturnsPhoneNUmber_String()
+    {
+      string expectedPhoneNumber = "5038905118";
+      Client newClient = new Client("Scott", "Bergler", expectedPhoneNumber, 1, 1);
+
+      string actualPhoneNumber = newClient.GetPhoneNumber();
+
+      Assert.AreEqual(expectedPhoneNumber, actualPhoneNumber);
+    }
+
+    [TestMethod]
+    public void GetStylistId_ReturnsStylistId_Int()
+    {
+      int expectedStylistId = 1;
+      Client newClient = new Client("Scott", "Bergler", "5038905118", expectedStylistId, 1);
+
+      int actualStylistId = newClient.GetStylistId();
+
+      Assert.AreEqual(expectedStylistId, actualStylistId);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_List()
+    {
+      List<Client> expectedClientList = new List<Client> { };
+
+      List<Client> actualClientList = Client.GetAll();
+
+      CollectionAssert.AreEqual(expectedClientList, actualClientList);
+    }
+
+    [TestMethod]
+    public void Save_SavesToDatabase_ClientList()
+    {
+      Client newClient = new Client("Scott", "Bergler", "5038905118", 1);
+      List<Client> expectedClientList = new List<Client>{newClient};
+
+      newClient.Save();
+      List<Client> actualClientList = Client.GetAll();
+
+      CollectionAssert.AreEqual(expectedClientList, actualClientList);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsAllClients_ClientList()
+    {
+      Client clientOne = new Client("Scott", "Bergler", "5038905118", 1);
+      Client clientTwo = new Client("Millicent", "Zimdars", "5034217832", 2);
+      List<Client> expectedClientList = new List<Client>{ clientOne, clientTwo };
+
+      clientOne.Save();
+      clientTwo.Save();
+      List<Client> actualClientList = Client.GetAll();
+
+      CollectionAssert.AreEqual(expectedClientList, actualClientList);
+    }
+
+    [TestMethod]
+    public void Save_AssignsId_Int()
+    {
+      Client clientOne = new Client("Scott", "Bergler", "5038905118", 1);
+      Client clientTwo = new Client("Millicent", "Zimdars", "5034217832", 2);
+      List<Client> expectedClientList = new List<Client>{ clientOne, clientTwo };
+
+      clientOne.Save();
+      clientTwo.Save();
+      List<Client> actualClientList = Client.GetAll();
+
+      Assert.AreEqual(expectedClientList[1].GetId(), actualClientList[1].GetId());
+    }
+
+    [TestMethod]
+    public void Find_ReturnsCorrectClientFromDatabase_Client()
+    {
+      Client clientOne = new Client("Scott", "Bergler", "5038905118", 1);
+      Client clientTwo = new Client("Millicent", "Zimdars", "5034217832", 2);
+
+      clientOne.Save();
+      clientTwo.Save();
+      Client foundClient = Client.Find(clientTwo.GetId());
+
+      Assert.AreEqual(clientTwo, foundClient);
+    }
 
   }
 }
